09bcc8b42f7b760f7fad68145b874638441d5cc0
Pascal Craponne <pascal@craponne.fr>
2015-05-22 21:01:57 +0200

    Added extensions to OnePath

---
+++ OneFilesystem/OnePathExtensions.cs
@@ -0,0 +1,28 @@
+﻿#region OneFilesystem
+// OneFilesystem
+// (to rule them all... Or at least some...)
+// https://github.com/ArxOne/OneFilesystem
+// Released under MIT license http://opensource.org/licenses/MIT
+#endregion
+
+namespace ArxOne.OneFilesystem
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Extensions to <see cref="OnePath"/>
+    /// </summary>
+    public static class OnePathExtensions
+    {
+        /// <summary>
+        /// Gets the self and ancestors.
+        /// </summary>
+        /// <param name="onePath">The one path.</param>
+        /// <returns></returns>
+        public static IEnumerable<OnePath> GetSelfAndAncestors(this OnePath onePath)
+        {
+            for (var currentPath = onePath; currentPath != null; currentPath = currentPath.GetParent())
+                yield return currentPath;
+        }
+    }
+}
